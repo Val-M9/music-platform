8d5fca460e43454a1fb1ceb8a5657a60b1677c43
Val_M <v.mischenko90@gmail.com>
2022-12-31 03:04:24 +0200

    add general methods to manipulate tracks

--- backend/src/track/track.controller.ts
+++ backend/src/track/track.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ObjectId } from 'mongoose';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { TrackService } from './track.service';
 
@@ -11,8 +13,33 @@ export class TrackController {
     return this.trackService.createTrack(createTrackDto);
   }
 
+  @Get('/search')
+  search(@Query('query') query: string) {
+    return this.trackService.search(query);
+  }
+
   @Get()
   getAll() {
     return this.trackService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: ObjectId) {
+    return this.trackService.getById(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: ObjectId) {
+    return this.trackService.delete(id);
+  }
+
+  @Post('/comment')
+  addComment(@Body() commentDto: CreateCommentDto) {
+    return this.trackService.addComment(commentDto);
+  }
+
+  @Post('/listens/:id')
+  incrementListens(@Param('id') id: ObjectId) {
+    return this.trackService.incrementListens(id);
   }
 }
--- backend/src/track/track.service.ts
+++ backend/src/track/track.service.ts
@@ -1,6 +1,7 @@
+import { CreateCommentDto } from './dto/create-comment.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId, Types } from 'mongoose';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { Track, TrackDocument } from './schemas/track.schema';
 import { Comment, CommentDocument } from './schemas/comment.schema';
@@ -18,9 +19,44 @@ export class TrackService {
     return track;
   }
 
+  async search(query: string): Promise<Track[]> {
+    const tracks = await this.trackModel.find({
+      name: { $regex: new RegExp(query, 'i') },
+    });
+
+    return tracks;
+  }
+
   async getAll(): Promise<Track[]> {
     const tracks = await this.trackModel.find();
 
     return tracks;
+  }
+
+  async getById(id: ObjectId): Promise<Track> {
+    const track = await this.trackModel.findById(id).populate('comments');
+
+    return track;
+  }
+
+  async delete(id: ObjectId): Promise<Types.ObjectId> {
+    const deletedTrack = await this.trackModel.findByIdAndDelete(id);
+
+    return deletedTrack._id;
+  }
+
+  async addComment(commentDto: CreateCommentDto): Promise<Comment> {
+    const track = await this.trackModel.findById(commentDto.trackId);
+    const comment = await this.commentModel.create({ ...commentDto });
+    track.comments.push(comment);
+    track.save();
+
+    return comment;
+  }
+
+  async incrementListens(id: ObjectId) {
+    const track = await this.trackModel.findById(id);
+    track.listens += 1;
+    track.save();
   }
 }
